aea6da77d636eb78d25e518cb0e7f51c4f7b9196
Ashish Bajracharya <lsr.files1@gmail.com>
2021-01-02 12:10:56 +0545

    slider added and styled

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import react from 'react'
 import Header from './components/header_footer/header'
+import Featured from './components/featured/featured'
 const App = () =>{
     return(
        <div className = "app" style={{
            height: '2000px'
        }}>
            <Header />
+           <Featured />
        </div>
     )
 }
